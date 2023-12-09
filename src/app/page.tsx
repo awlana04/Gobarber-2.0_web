4d04234e7df3055d658c54a4c49d6c06ad50c4c5
Awlana Costa <long0413@pop-os.localdomain>
2023-12-09 00:03:55 -0300

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,38 @@
+import Image from 'next/image';
+
+import gobarberImage001 from '../../public/gobarber_image001.svg';
+import gobarberLogo001 from '../../public/gobarber_logo001.svg';
+
 export default function Home() {
-  return <h1>Go Barber 2.0!</h1>;
+  return (
+    <body>
+      <Image
+        src={gobarberImage001}
+        alt='Foto de barbearia'
+        className='h-screen w-min'
+      />
+
+      <main className='grid w-screen'>
+        <Image
+          src={gobarberLogo001}
+          alt='Logo do Gobarber'
+          className='m-auto mt-16'
+        />
+
+        <p className='m-auto w-[45%]'>
+          O GoBarber é uma aplicação voltada para barbeiros(a) e homens à
+          procura de um ótimo profissional para cuidar da sua beleza masculina.
+          <br />
+          <br />
+          Aqui você pode começar criando a sua conta como usuário e fazer o seu
+          primeiro agendamento, ou iniciar como um barbeiro e ir acompanhanto as
+          demandas.
+        </p>
+
+        <button className='m-auto mt-4'>Fazer logon</button>
+
+        <span className='m-auto'>Criar conta</span>
+      </main>
+    </body>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,24 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      orange: '#FF9000',
+      white: '#F4EDE8',
+      buttonText: '#312E38',
+      input: '#232129',
+      background: '#312E38',
+      red: '#FF0000',
+      black: '#28262E',
+      textHover: '#FFFFFF',
     },
+    // extend: {
+    //   backgroundImage: {
+    //     'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+    //     'gradient-conic':
+    //       'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    //   },
+    // },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
