0aa2cc1ba2b8470894efbdada47c509d47fc2139
Awlana Costa <cawlana040@gmail.com>
2024-01-09 15:05:47 -0300

    content template test

---
+++ src/presentation/templates/content-template.spec.tsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+
+import ContentTemplate from './content-template';
+
+describe('Content template', () => {
+  it('should be able to render the content template', () => {
+    const { getByText } = render(
+      <ContentTemplate position='left' src='/image.png' alt='Image'>
+        Template
+      </ContentTemplate>
+    );
+
+    const ContentTemplateElement = getByText('Template');
+
+    expect(ContentTemplateElement).toBeInTheDocument();
+  });
+
+  it('should not be able to render the content template', () => {
+    const { queryByText } = render(
+      <ContentTemplate position='left' src='/image' alt='Imagem da barbearia'>
+        Template
+      </ContentTemplate>
+    );
+
+    const ContentTemplateElement = queryByText('Template');
+
+    expect(ContentTemplateElement).not.toBeInTheDocument();
+  });
+});
