3dc4c8486cc3c2fb7047985cb530f238cf2de074
Awlana Costa <cawlana040@gmail.com>
2024-01-20 16:31:04 -0300

    logon screen test

--- src/presentation/screens/logon-screen.spec.tsx
+++ src/presentation/screens/logon-screen.spec.tsx
@@ -1,9 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import LogonScreen from './logon-screen';
 
 describe('LogonScreen', () => {
-  // it('should be able to render the logon screen', () => {
-  //   const {} = render(<LogonScreen />);
-  // });
+  it('should be able to render the logon screen', () => {
+    render(
+      <LogonScreen
+        submitHandler={() => {}}
+        emailInput={{
+          submitField: 'email',
+          errored: false,
+        }}
+        passwordInput={{
+          submitField: 'password',
+          errored: false,
+        }}
+      />
+    );
+
+    const logonScreenElement = screen.getByRole('textbox', {
+      name: 'E-mail',
+    });
+
+    expect(logonScreenElement).toBeVisible();
+  });
 });
