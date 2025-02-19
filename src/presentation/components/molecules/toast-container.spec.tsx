21a256e776380bc5518c74486fdde577f7863d99
awlana04 <cawlana040@gmail.com>
2025-02-18 22:48:23 -0300

    toast container test created

---
+++ src/presentation/components/molecules/toast-container.spec.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+
+import ToastContainer from './toast-container';
+
+describe('<ToastContainer> component', () => {
+  beforeEach(() => {
+    render(
+      <ToastContainer
+        messages={[
+          {
+            id: '12345678',
+            type: 'success',
+            title: 'Hello World!',
+            description: 'Everything alright!',
+          },
+        ]}
+      />
+    );
+  });
+
+  it('should be able to render the toast container component', () => {
+    const titleToastContainerElement = screen.getByText('Hello World!');
+    const descriptionToastContainerElement = screen.getByText(
+      'Everything alright!'
+    );
+
+    expect(titleToastContainerElement).toBeVisible();
+    expect(descriptionToastContainerElement).toBeVisible();
+  });
+
+  it('should not be able to render the toast container component', () => {
+    const wrongTitleToastContainerElement = screen.queryByText('wrong title!');
+
+    expect(wrongTitleToastContainerElement).toBeNull();
+  });
+});
