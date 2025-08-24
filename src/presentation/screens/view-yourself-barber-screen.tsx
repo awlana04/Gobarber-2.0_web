161a70957a524e7786f68148696741d624ea3aca
awlana04 <cawlana040@gmail.com>
2025-08-24 15:08:37 -0300

    view yourself barber screen image carrousel implemented

--- src/presentation/screens/view-yourself-barber-screen.tsx
+++ src/presentation/screens/view-yourself-barber-screen.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import { useMemo, useState } from 'react';
 import Image from 'next/image';
-import { FiCalendar, FiClock } from 'react-icons/fi';
+import {
+  FiCalendar,
+  FiChevronLeft,
+  FiChevronRight,
+  FiClock,
+} from 'react-icons/fi';
 
 import { HeaderPropsType } from '@/presentation/types/header-props-type';
 import { BarberDataType } from '@/infra/types/data-type';
@@ -18,6 +24,59 @@ type ViewYourselfBarberScreenPropsType = HeaderPropsType & {
 export default function ViewYourselfBarberScreen(
   props: ViewYourselfBarberScreenPropsType
 ) {
+  const [activeImages, setActiveImages] = useState<[string, string, string]>([
+    '',
+    '',
+    '',
+  ]);
+
+  // Take the first 3 elements in the barber images array
+  const barberImagesArray = props.barber.images!.slice(0, 3);
+
+  // Save the initial carrousel images state
+  useMemo(() => {
+    setActiveImages([
+      barberImagesArray[0],
+      barberImagesArray[1],
+      barberImagesArray[2],
+    ]);
+  }, []);
+
+  // Find the barber image index corresponding to the activeImages array last element
+  const barberImageIndex = props.barber.images!.findIndex(
+    (image) => image === activeImages[2]
+  );
+
+  const nextImage = () => {
+    // With the barberImageIndex index, we create a new array beginning from index of the last visible image and ending in exact index of the new visible image on the right
+    const newBarberImagesArrayIndex = props.barber.images!.slice(
+      barberImageIndex - 1,
+      barberImageIndex + 2
+    );
+
+    // Set the new carrousel state according to the newBarberImagesArrayIndex
+    setActiveImages([
+      newBarberImagesArrayIndex[0],
+      newBarberImagesArrayIndex[1],
+      newBarberImagesArrayIndex[2],
+    ]);
+  };
+
+  const previousImage = () => {
+    // With the barberImageIndex index, we create a new array beginning from index of the last visible image and ending in exact previous index of the visible image from the left
+    const newBarberImagesArrayIndex = props.barber.images!.slice(
+      barberImageIndex - 3,
+      barberImageIndex
+    );
+
+    // Set the new carrousel state according to the newBarberImagesArrayIndex
+    setActiveImages([
+      newBarberImagesArrayIndex[0],
+      newBarberImagesArrayIndex[1],
+      newBarberImagesArrayIndex[2],
+    ]);
+  };
+
   return (
     <DashboardTemplate {...props} headerType='dashboard'>
       <div className='mx-48 mt-16 flex flex-row space-x-8'>
@@ -46,17 +105,42 @@ export default function ViewYourselfBarberScreen(
         </div>
       </div>
 
-      <div className='mt-28 flex flex-row items-center justify-center'>
+      <div className='mt-24 flex flex-row items-center justify-center px-10'>
+        {barberImagesArray.findIndex((image) => image) !==
+          props.barber.images!.findIndex(
+            (image) => image === activeImages[0]
+          ) && (
+          <div
+            onClick={previousImage}
+            className='bg-orange mr-4 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full'
+          >
+            <FiChevronLeft className='text-3xl' />
+          </div>
+        )}
+
         {props.barber.images &&
-          props.barber.images.map((image) => (
+          activeImages.map((image) => (
             <Image
               key={image}
               src={`${process.env.NEXT_PUBLIC_BACKEND_URI}/files/${image}`}
               alt='Imagem da barbearia'
-              width={144}
-              height={144}
+              width={312}
+              height={312}
+              className='mx-2 rounded-2xl'
             />
           ))}
+
+        {props.barber.images!.length - 1 !==
+          props.barber.images!.findIndex(
+            (image) => image === activeImages[2]
+          ) && (
+          <div
+            onClick={nextImage}
+            className='bg-orange ml-4 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full'
+          >
+            <FiChevronRight className='text-3xl' />
+          </div>
+        )}
       </div>
     </DashboardTemplate>
   );
